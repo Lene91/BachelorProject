d2c8a57a891b4415e99dccdd29bfbc0888a0822e
Lene <lene.ganschow@online.de>
2014-05-04 12:11:49 +0200

    unwichtige Dateien gelöscht, Verweise hinzugefügt, main-Methode erstellt

---
+++ BachelorProject/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using ExperimentTemplate;
+using Eyetracker.MouseTracker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BachelorProject
+{
+    class EntryPoint
+    {
+        [STAThread]
+        static void Main()
+        {
+            var Tracker = new MouseTracker();
+            var experiment = new Experiment(Tracker);
+            experiment.Run();
+        }
+    }
+}
