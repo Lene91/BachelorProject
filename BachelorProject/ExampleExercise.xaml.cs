07813dd77acc1f6d5efb50f59455eda8c1ee0d93
Lene <lene.ganschow@online.de>
2014-05-04 19:16:01 +0200

    Element kann verschoben werden

--- BachelorProject/ExampleExercise.xaml.cs
+++ BachelorProject/ExampleExercise.xaml.cs
@@ -12,17 +12,74 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace BachelorProject
 {
     /// <summary>
     /// Interaktionslogik für ExampleExercise.xaml
+    /// Quelle für Dragging-Interaktion: http://denismorozov.blogspot.de/2008/01/drag-controls-in-wpf-using.html
     /// </summary>
     public partial class ExampleExercise : UserControl
     {
+        private Element current = new Element();
+
         public ExampleExercise()
         {
             InitializeComponent();
         }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.current.X = Mouse.GetPosition((IInputElement)sender).X;
+            this.current.Y = Mouse.GetPosition((IInputElement)sender).Y;
+
+            // Ensure object receives all mouse events.
+            this.current.InputElement.CaptureMouse();
+        }
+
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (this.current.InputElement != null)
+                this.current.InputElement.ReleaseMouseCapture();
+            this.current.IsDragging = false;
+        }
+
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            // if mouse is down when its moving, then it's dragging current
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.current.IsDragging = true;
+
+            if (this.current.IsDragging && current.InputElement != null)
+            {
+                // Retrieve the current position of the mouse.
+                var newX = Mouse.GetPosition((IInputElement)sender).X;
+                var newY = Mouse.GetPosition((IInputElement)sender).Y;
+  
+                // Reset the location of the object (add to sender's renderTransform
+                // newPosition minus currentElement's position
+
+                var rt = ((UIElement)this.current.InputElement).RenderTransform;
+                var offsetX = rt.Value.OffsetX;
+                var offsetY = rt.Value.OffsetY;
+                rt.SetValue(TranslateTransform.XProperty, offsetX + newX - current.X);
+                rt.SetValue(TranslateTransform.YProperty, offsetY + newY - current.Y);
+
+                // Update position of the mouse
+                current.X = newX;
+                current.Y = newY;
+            }
+        }
+
+
+        private void ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.current.InputElement = (IInputElement)sender;
+        }
+
     }
+
 }
